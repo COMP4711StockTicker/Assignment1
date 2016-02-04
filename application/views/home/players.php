90b7b9e415207afeb6b61630977ea430e4c2df33
HsuanWei Fu <hsuanweifu@gmail.com>
2016-02-03 16:08:50 -0800

    made master/navbar/homepage

---
+++ application/views/home/players.php
@@ -0,0 +1,24 @@
+<div id="players" class="tab-pane fade in fade">
+    <div class = "row">
+        <div class = "col-md-10 col-md-offset-1">
+            <div class = "table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>Equity</th>
+                        <th>Cash</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td><a href="/portfolio/1">Big Dick</a></td>
+                        <td>1000</td>
+                        <td>100</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
